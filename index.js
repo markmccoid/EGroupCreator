6ee1f289f4fff1be5ed65bd30d730ede77e529fc
Mark McCoid <markmccoid@gmail.com>
2017-07-14 13:53:34 -0400

    update dev tools dir

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.on('ready', () => {
 //--Set up dev tools if in development mode development is for work computer, dev-home is for home computer
 console.log(process.env.NODE_ENV);
 	if (process.env.NODE_ENV === 'development') {
-		BrowserWindow.addDevToolsExtension('C:/Users/mark.mccoid/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.4.0_0');
+		BrowserWindow.addDevToolsExtension('C:/Users/mark.mccoid/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.5.0_2');
 		BrowserWindow.addDevToolsExtension('C:/Users/mark.mccoid/AppData/Local/Google/Chrome/User Data/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.15.1_0');
 	} else if (process.env.NODE_ENV === 'dev-home') {
 		BrowserWindow.addDevToolsExtension('C:/Users/mark/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/2.4.0_0');
